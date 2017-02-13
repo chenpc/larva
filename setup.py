94ac37641d6d92dfb0c856efb19ecb89f59fbbd1
Chen Wen <pokkys@gmail.com>
2017-02-13 17:07:49 +0800

    Add package_data

--- setup.py
+++ setup.py
@@ -53,7 +53,8 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'larva': ['static/*/*', 'templates/*'],
+        'static': ['*'],
+        'templates': ['*'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
