4b3cd82e0e7ffa12712a3db53b2f5fb61901bbf7
root <root@localhost.localdomain>
2017-02-14 15:10:56 +0800

    fix install path

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
@@ -53,8 +52,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'static': ['*'],
-        'templates': ['*'],
+        'larva': ["templates", "static"],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
