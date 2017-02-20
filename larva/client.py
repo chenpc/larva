3bda4e6afd5bce9ccf6dbda7cdf984b317784951
chenpc <pokkys@gmail.com>
2017-02-21 00:41:56 +0800

    Initiate token while __init__

--- larva/client.py
+++ larva/client.py
@@ -2,30 +2,32 @@ import requests
 from functools import partial
 
 
-r = requests.post('http://127.0.0.1:8080/auth', auth=('user', 'pass'))
-token = r.text
-
-
-def larva_call(module, func, *args, **kwargs):
-    global token
+def larva_call(host, port, token, module, func, *args, **kwargs):
     headers = {'Authorization': 'Token %s' % token, "Content-Type": "application/json"}
     params = [args, kwargs]
-    result = requests.post('http://127.0.0.1:8080/api/%s/%s' % (module, func), headers=headers, json=params)
-    return result.text
+    result = requests.post('http://%s:%d/api/%s/%s' % (host, port, module, func), headers=headers, json=params)
+    return result.json()
 
 
 class LarvaProxy(object):
-    def __init__(self, method=None):
+    def __init__(self, host="127.0.0.1", port=8080, token=None, method=None):
+        if token is None:
+            r = requests.post('http://%s:%d/auth' % (host, port), auth=('user', 'pass'))
+            self.token = r.text
+        else:
+            self.token = token
         self.method = method
+        self.host = host
+        self.port = port
 
     def __getattr__(self, item):
         if self.method:
-            return partial(larva_call, self.method, item)
+            return partial(larva_call, self.host, self.port, self.token, self.method, item)
         else:
-            return LarvaProxy(item)
+            return LarvaProxy(self.host, self.port, self.token, method=item)
 
 
-rpc = LarvaProxy()
+rpc = LarvaProxy("127.0.0.1", 8080)
 res = rpc.hello.test_hello("kk", True, 4)
 print(res)
 
