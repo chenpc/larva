6af678f00d481a2cff81307e543c8c0f73c7a406
Chen Wen <pokkys@gmail.com>
2017-07-21 20:06:37 +0800

    Change Client to https

--- larva/client.py
+++ larva/client.py
@@ -15,7 +15,7 @@ class LarvaError(Exception):
 def larva_call(host, port, token, module, func, *args, **kwargs):
     headers = {'Authorization': 'Token %s' % token, "Content-Type": "application/json"}
     params = [args, kwargs]
-    result = requests.post('http://%s:%d/api/%s/%s' % (host, port, module, func), headers=headers, json=params)
+    result = requests.post('https://%s:%d/api/%s/%s' % (host, port, module, func), headers=headers, json=params)
     result = json.loads(result.text, object_pairs_hook=OrderedDict)
 
     if result['status']:
@@ -31,7 +31,7 @@ def larva_call(host, port, token, module, func, *args, **kwargs):
 class LarvaProxy(object):
     def __init__(self, host="127.0.0.1", port=80, username=None, password=None, token=None, method=None):
         if token is None:
-            r = requests.post('http://%s:%d/auth' % (host, port), auth=(username, password))
+            r = requests.post('https://%s:%d/auth' % (host, port), auth=(username, password))
             self.token = r.text
         else:
             self.token = token
