df6ddff1c5ef42c77c92d3f4d18789d3944a88a3
Chen Wen <pokkys@gmail.com>
2017-07-21 20:10:37 +0800

    set default to 443 port

--- larva/client.py
+++ larva/client.py
@@ -29,7 +29,7 @@ def larva_call(host, port, token, module, func, *args, **kwargs):
 
 
 class LarvaProxy(object):
-    def __init__(self, host="127.0.0.1", port=80, username=None, password=None, token=None, method=None):
+    def __init__(self, host="127.0.0.1", port=443, username=None, password=None, token=None, method=None):
         if token is None:
             r = requests.post('https://%s:%d/auth' % (host, port), auth=(username, password))
             self.token = r.text
