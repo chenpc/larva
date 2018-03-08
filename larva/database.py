3c9a33bb3c92154f8278644e6647e8320a05e075
Wen Chen- PTT 陳文 <wen.chen@tw.promise.com>
2018-03-08 19:17:38 +0800

    Change db location for yocto

--- larva/database.py
+++ larva/database.py
@@ -4,16 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 import sys
 
-if sys.platform.startswith('win32'):
-    this_file = os.path.abspath(__file__)
-    this_dir = os.path.dirname(this_file)
-    if not os.path.exists(this_dir + '\db'):
-        os.makedirs(this_dir + '\db')
-    engine = create_engine('sqlite:///' + this_dir + '\db\larva.db', convert_unicode=True)
+if os.path.exists('/media/realroot'):
+    engine = create_engine('sqlite:////media/realroot/larva.db', convert_unicode=True)
 else:
-    if not os.path.exists('/var/db'):
-        os.makedirs('/var/db')
-    engine = create_engine('sqlite:////var/db/larva.db', convert_unicode=True)
+    engine = create_engine('sqlite:////tmp/larva.db', convert_unicode=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
